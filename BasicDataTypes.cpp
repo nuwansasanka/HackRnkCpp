b397d803aaf7186bb605a2db870ca5b14f2eb6ff
Akila De Silva <54980465+AkilaDee@users.noreply.github.com>
2020-09-11 17:24:14 +0530

    Create BasicDataTypes.cpp

---
+++ BasicDataTypes.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    
+    long b;
+    
+    char c;
+    float n;
+    double d;
+
+    cin >> a>>b>>c>>n>>d;
+    cout << a << endl;
+   
+    cout << b << endl;
+   
+    cout << c << endl;
+    
+    cout <<fixed<< n << endl;
+    
+
+    cout <<fixed<< d << endl;
+    return 0;
+}
